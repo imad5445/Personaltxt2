1bc4cc245637ab75b38d2ff0129d36ae7fcc5a38
imad5445 <157965306+imad5445@users.noreply.github.com>
2024-02-16 02:26:10 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
-api_id = "27327455"
-api_hash = "0b60d99056b4d168b1bd717aeb7a699b"
-bot_token = "6884087876:AAFbnPGPxIVtBAJkYizWXoW1qnP5tM6ktH8"
+api_id = "24218997"
+api_hash = "b0419e875ab8647b12e885e74c492297"
+bot_token = "6637358947:AAEoa3tHaRiPaQisYu-qxUcIH52yoidzbvg"
